1da805a352866329635375630a8de40ffd7ab794
nag763 <loic.labeye@tutanota.de>
2022-10-16 17:16:01 +0200

    Collision event added, but it's slow af

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,31 @@
 use bevy::time::FixedTimestep;
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::collide_aabb::collide, sprite::MaterialMesh2dBundle};
 
 const PADDLE_SPEED_FACTOR: f32 = 5.;
+const PADDLE_DISTANCE_FACTOR: f32 = 0.4;
 const BALL_DIAMETER: f32 = 32.;
 const SCREEN_HEIGHT: f32 = 720.;
 const SCREEN_WIDTH: f32 = 480.;
-const TIME_STEP: f64 = 0.01;
+const TIME_STEP: f64 = 0.0001;
+const BALL_TRANSLATION_PER_STEP: f32 = 0.025;
+
+const PADDLE_DIMENSIONS: Vec2 = Vec2 {
+    x: 128.,
+    y: 0.175 * 128.,
+};
+const HIGHER_PADDLE_Y_AXIS: f32 = SCREEN_HEIGHT * PADDLE_DISTANCE_FACTOR;
+const LOWER_PADDLE_Y_AXIS: f32 = -HIGHER_PADDLE_Y_AXIS;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0., 0., 0.)))
+        .insert_resource(ClearColor(Color::BLACK))
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(window_resize_system)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP))
-                .with_system(ball_fall)
+                .with_system(check_bounds)
                 .with_system(ball_velocity),
         )
         .add_system(paddle_move_system)
@@ -32,23 +41,28 @@ fn setup(
     commands
         .spawn()
         .insert(Paddle)
+        .insert(Collider)
         .insert_bundle(MaterialMesh2dBundle {
             mesh: meshes
                 .add(Mesh::from(shape::Quad {
-                    size: Vec2 { x: 1., y: 0.25 },
+                    size: PADDLE_DIMENSIONS,
                     flip: true,
                 }))
                 .into(),
-            transform: Transform::default().with_scale(Vec3::splat(128.)),
+            transform: Transform::default().with_translation(Vec3 {
+                y: LOWER_PADDLE_Y_AXIS,
+                ..Vec3::default()
+            }),
             material: materials.add(ColorMaterial::from(Color::WHITE)),
             ..default()
-        });
+        })
+        .insert(Collider);
     commands
         .spawn()
         .insert(Ball)
         .insert(BallVelocity {
             direction: -1.,
-            speed: 0.,
+            speed: BALL_TRANSLATION_PER_STEP,
         })
         .insert_bundle(MaterialMesh2dBundle {
             mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
@@ -69,23 +83,42 @@ struct Paddle;
 #[derive(Component)]
 struct Ball;
 
+#[derive(Component)]
+struct Collider;
+
 #[derive(Component, Default)]
 struct BallVelocity {
     direction: f32,
     speed: f32,
 }
 
-fn ball_fall(
-    time: Res<Time>,
-    mut ball_query: Query<(&mut Transform, &mut BallVelocity), With<Ball>>,
+fn check_bounds(
+    mut commands: Commands,
+    mut set: ParamSet<(
+        Query<(&mut Transform, Entity), With<Ball>>,
+        Query<&Transform, With<Collider>>,
+    )>,
 ) {
-    for (transform, mut velocity) in &mut ball_query {
-        let y_position = transform.translation.y;
-        if f32::abs(y_position) < (SCREEN_HEIGHT - BALL_DIAMETER) / 2. {
-            velocity.speed = time.seconds_since_startup() as f32;
-        } else {
-            velocity.direction *= -1.;
-        }
+    let paddle = set.p1();
+    let paddle_transform = paddle.single();
+    let paddle_position = paddle_transform.translation;
+
+    let mut ball = set.p0();
+    let (mut transform, entity) = ball.single_mut();
+    let (x_position, y_position) = (transform.translation.x, transform.translation.y);
+    let collide = collide(
+        transform.translation,
+        Vec2::splat(BALL_DIAMETER),
+        paddle_position,
+        PADDLE_DIMENSIONS,
+    );
+    if collide.is_some() {
+        transform.translation.y = HIGHER_PADDLE_Y_AXIS;
+    } else if HIGHER_PADDLE_Y_AXIS < f32::abs(y_position) {
+        commands.entity(entity).despawn();
+    }
+    if SCREEN_WIDTH < f32::abs(x_position) / 2. {
+        todo!();
     }
 }
 
@@ -100,16 +133,14 @@ fn paddle_move_system(
     mut query: Query<&mut Transform, With<Paddle>>,
 ) {
     let mut paddle_transform = query.single_mut();
+    let mut new_x_paddle = paddle_transform.translation.x;
     if keyboard_input.pressed(KeyCode::Right) {
-        paddle_transform.translation.x += 1.0 * PADDLE_SPEED_FACTOR;
+        new_x_paddle += 1.0 * PADDLE_SPEED_FACTOR;
     }
     if keyboard_input.pressed(KeyCode::Left) {
-        paddle_transform.translation.x -= 1.0 * PADDLE_SPEED_FACTOR;
+        new_x_paddle -= 1.0 * PADDLE_SPEED_FACTOR;
     }
-    if keyboard_input.pressed(KeyCode::Up) {
-        paddle_transform.translation.y += 1.0 * PADDLE_SPEED_FACTOR;
-    }
-    if keyboard_input.pressed(KeyCode::Down) {
-        paddle_transform.translation.y -= 1.0 * PADDLE_SPEED_FACTOR;
+    if f32::abs(new_x_paddle) < (SCREEN_WIDTH - PADDLE_DIMENSIONS.x) / 2. {
+        paddle_transform.translation.x = new_x_paddle;
     }
 }
