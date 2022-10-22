1527b441a44ad8dc12bcf0a51af4a5448dc4030e
nag763 <loic.labeye@tutanota.de>
2022-10-22 23:38:57 +0200

    Hey look at me it works !

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ const SCREEN_WIDTH: f32 = 480.;
 const TIME_STEP: f64 = 0.01;
 const BALL_TRANSLATION_PER_STEP: f32 = 2.5;
 const BALL_DEFECTION_FACTOR: f32 = 40.;
-const BORDER_SPLAT_SIZE : f32 = 1.0f32;
+const BORDER_SPLAT_SIZE: f32 = 1.0f32;
 
 const PADDLE_DIMENSIONS: Vec2 = Vec2 {
     x: 128.,
@@ -21,17 +21,21 @@ const LOWER_PADDLE_Y_AXIS: f32 = -HIGHER_PADDLE_Y_AXIS;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
+        .insert_resource(ScoreSheet::default())
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .add_system(window_resize_system)
+        .add_event::<OutOfBoundsEvent>()
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP))
                 .with_system(check_bounds)
                 .with_system(lower_paddle_move_system.before(check_bounds))
                 .with_system(higher_paddle_move_system.before(check_bounds))
-                .with_system(ball_velocity.before(check_bounds)),
+                .with_system(ball_velocity.before(check_bounds))
+                .with_system(out_of_bounds_event.before(check_bounds)),
         )
+        .add_system(update_scoresheet)
         .run();
 }
 
@@ -39,8 +43,49 @@ fn setup(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>
 ) {
     commands.spawn_bundle(Camera2dBundle::default());
+    commands.spawn_bundle(
+        TextBundle::from_sections([
+            TextSection::from_style(
+                TextStyle {
+                    font_size: 25.,
+                    color:Color::BLUE,
+                    font: asset_server.load("score_font.otf"),
+                    ..default()
+                },
+            )]).with_style(
+                Style {
+                    position_type: PositionType::Absolute,
+                    position: UiRect {
+                        top: Val::Px(5f32),
+                        left: Val::Px(5f32),
+                        ..default()
+                    },
+                    ..default()
+                }),
+        );
+    commands.spawn_bundle(
+        TextBundle::from_sections([
+            TextSection::from_style(
+                TextStyle {
+                    font_size: 25.,
+                    color:Color::RED,
+                    font: asset_server.load("score_font.otf"),
+                    ..default()
+                },
+            )]).with_style(
+                Style {
+                    position_type: PositionType::Absolute,
+                    position: UiRect {
+                        bottom: Val::Px(5f32),
+                        right: Val::Px(5f32),
+                        ..default()
+                    },
+                    ..default()
+                }),
+        );
     commands
         .spawn()
         .insert(LowerPaddle)
@@ -55,7 +100,7 @@ fn setup(
                 y: LOWER_PADDLE_Y_AXIS,
                 ..Vec3::default()
             }),
-            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            material: materials.add(ColorMaterial::from(Color::RED)),
             ..default()
         })
         .insert(Collider);
@@ -73,7 +118,7 @@ fn setup(
                 y: HIGHER_PADDLE_Y_AXIS,
                 ..Vec3::default()
             }),
-            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            material: materials.add(ColorMaterial::from(Color::BLUE)),
             ..default()
         })
         .insert(Collider);
@@ -102,6 +147,7 @@ fn setup(
                     scale: Vec3::splat(BORDER_SPLAT_SIZE),
                     ..default()
                 },
+                visibility: Visibility { is_visible: false },
                 ..default()
             })
             .insert(Collider);
@@ -132,6 +178,15 @@ struct Border;
 #[derive(Component)]
 struct Collider;
 
+#[derive(Default, Component)]
+struct OutOfBoundsEvent;
+
+#[derive(Default, Debug)]
+struct ScoreSheet {
+    red: u32,
+    blue: u32,
+}
+
 #[derive(Component, Default)]
 struct BallVelocity {
     direction: f32,
@@ -142,6 +197,7 @@ struct BallVelocity {
 fn check_bounds(
     mut ball: Query<(&Transform, &mut BallVelocity), With<Ball>>,
     collider: Query<(&Transform, Option<&Border>), With<Collider>>,
+    mut oob_event_writer : EventWriter<OutOfBoundsEvent>,
 ) {
     let (transform, mut velocity) = ball.single_mut();
     let (ball_x, ball_y) = (transform.translation.x, transform.translation.y);
@@ -151,25 +207,30 @@ fn check_bounds(
 
         let dimensions = match maybe_border {
             Some(_) => Vec2::splat(BORDER_SPLAT_SIZE),
-            None => PADDLE_DIMENSIONS
+            None => PADDLE_DIMENSIONS,
         };
 
         let collide = collide(
             transform.translation,
             Vec2::splat(BALL_DIAMETER),
             paddle_position,
-            dimensions
+            dimensions,
         );
         if collide.is_some() {
             let (paddle_x, paddle_y) = (paddle_position.x, paddle_position.y);
             if maybe_border.is_none() {
                 velocity.direction *= -1.;
                 velocity.speed *= 1.1;
-                velocity.angle = Vec2::new(paddle_x, paddle_y).angle_between(Vec2::new(ball_x, ball_y)) * BALL_DEFECTION_FACTOR;
+                velocity.angle = Vec2::new(paddle_x, paddle_y)
+                    .angle_between(Vec2::new(ball_x, ball_y))
+                    * BALL_DEFECTION_FACTOR;
             } else {
-                velocity.angle*=-1.;
-            }   
+                velocity.angle *= -1.;
+            }
         }
+    }
+    if SCREEN_HEIGHT/2. < f32::abs(ball_y)  {
+        oob_event_writer.send_default();
     }
 }
 
@@ -212,4 +273,34 @@ fn higher_paddle_move_system(
     if f32::abs(new_x_paddle) < (SCREEN_WIDTH - PADDLE_DIMENSIONS.x) / 2. {
         paddle_transform.translation.x = new_x_paddle;
     }
+}
+
+fn update_scoresheet(scoresheet: Res<ScoreSheet>, mut query: Query<&mut Text>) {
+    for (i, mut text) in query.iter_mut().enumerate() {
+        if i == 0 {
+            text.sections[0].value = scoresheet.blue.to_string();
+        } else if i == 1 {
+            text.sections[0].value = scoresheet.red.to_string();
+        }
+    }
+}
+
+fn out_of_bounds_event(
+        oob_events: EventReader<OutOfBoundsEvent>,
+        mut ball_query: Query<(&mut Transform, &mut BallVelocity), With<Ball>>,
+        mut scoresheet : ResMut<ScoreSheet>,
+    ) {
+    if !oob_events.is_empty() {
+        oob_events.clear();
+        let (mut transform, mut velocity) = ball_query.single_mut();
+        if transform.translation.y < 0f32 {
+            scoresheet.blue += 1;
+        } else {
+            scoresheet.red += 1;
+        }
+        (transform.translation.x, transform.translation.y) = (0., 0.);
+        velocity.angle = 0.;
+        velocity.speed = BALL_TRANSLATION_PER_STEP;
+    }
+    
 }
