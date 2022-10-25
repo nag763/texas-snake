ba4e78212d1b80272cc79238261b3aad5d981de1
nag763 <loic.labeye@tutanota.de>
2022-10-25 20:26:57 +0200

    Well, it's dirty, but it works

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,17 +1,30 @@
+use rand::Rng;
+
 use bevy::{
-    prelude::*, sprite::collide_aabb::collide, sprite::MaterialMesh2dBundle, time::FixedTimestep, app::AppExit,
+    app::AppExit, prelude::*, sprite::collide_aabb::collide, sprite::MaterialMesh2dBundle,
+    time::FixedTimestep,
 };
 
 /// How many times per seconds the system does an action.
 const TIME_STEP: f64 = 0.01;
+
 /// The screen height.
 const SCREEN_HEIGHT: f32 = 480.;
+const MAX_SCREEN_HEIGHT: f32 = SCREEN_HEIGHT / 2.;
+const MIN_SCREEN_HEIGHT: f32 = -MAX_SCREEN_HEIGHT;
+
 /// The screen width.
 const SCREEN_WIDTH: f32 = 640.;
+const MAX_SCREEN_WIDTH: f32 = SCREEN_WIDTH / 2.;
+const MIN_SCREEN_WIDTH: f32 = -MAX_SCREEN_WIDTH;
 
-const SNAKE_SIZE: Vec2 = Vec2::splat(10.);
+const SNAKE_SIZE: f32 = 10f32;
+const SNAKE_DIMENSIONS: Vec2 = Vec2::splat(SNAKE_SIZE);
 
-const BORDER_SIZE: Vec2 = Vec2::splat(1.);
+const BORDER_SIZE: f32 = 10f32;
+const BORDER_DIMENSIONS: Vec2 = Vec2::splat(BORDER_SIZE);
+
+const BONUS_DIAMETER: f32 = 10.;
 
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
 enum SnakeDirection {
@@ -32,10 +45,10 @@ impl SnakeDirection {
     }
 
     fn conflicts_with(&self, other: Self) -> bool {
-        &self.get_conflictual_direction() == &other
+        self.get_conflictual_direction() == other
     }
 
-    fn into_translation(&self) -> Vec3 {
+    fn into_translation(self) -> Vec3 {
         match self {
             Self::Up => Vec3::new(0., 1., 0.),
             Self::Down => Vec3::new(0., -1., 0.),
@@ -58,7 +71,23 @@ struct Border;
 struct Collider;
 
 #[derive(Default)]
-struct CollisionEvent;
+enum CollisionEvent {
+    #[default]
+    Border,
+    Bonus(u32),
+}
+
+#[derive(Component, Default)]
+struct Bonus {
+    extra_bonus: bool,
+}
+
+fn compute_random_translation_inside(x0: f32, x1: f32, y0: f32, y1: f32) -> Vec3 {
+    let mut rng = rand::thread_rng();
+    let x = rng.gen_range(x0..x1);
+    let y = rng.gen_range(y0..y1);
+    Vec3 { x, y, ..default() }
+}
 
 fn main() {
     App::new()
@@ -72,9 +101,9 @@ fn main() {
                 .with_run_criteria(FixedTimestep::step(TIME_STEP))
                 .with_system(change_snake_direction)
                 .with_system(check_collisions)
+                .with_system(collision_handler)
                 .with_system(move_snake.before(change_snake_direction)),
         )
-        .add_system(game_over)
         .run();
 }
 
@@ -83,6 +112,12 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
 ) {
+    let bonus_initial_position = compute_random_translation_inside(
+        MIN_SCREEN_WIDTH + BORDER_SIZE,
+        MAX_SCREEN_WIDTH - BORDER_SIZE,
+        MIN_SCREEN_HEIGHT + BORDER_SIZE,
+        MAX_SCREEN_HEIGHT - BORDER_SIZE,
+    );
     // The camera
     commands.spawn_bundle(Camera2dBundle::default());
     commands
@@ -91,7 +126,7 @@ fn setup(
         .insert_bundle(MaterialMesh2dBundle {
             mesh: meshes
                 .add(Mesh::from(shape::Quad {
-                    size: SNAKE_SIZE,
+                    size: SNAKE_DIMENSIONS,
                     ..default()
                 }))
                 .into(),
@@ -99,6 +134,18 @@ fn setup(
             material: materials.add(ColorMaterial::from(Color::WHITE)),
             ..default()
         });
+    commands
+        .spawn()
+        .insert(Bonus::default())
+        .insert_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
+            transform: Transform::default()
+                .with_scale(Vec3::splat(BONUS_DIAMETER))
+                .with_translation(bonus_initial_position),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            ..default()
+        })
+        .insert(Collider);
 
     let mut border_maker_closure = |x: f32, y: f32| {
         commands
@@ -107,7 +154,7 @@ fn setup(
             .insert_bundle(MaterialMesh2dBundle {
                 mesh: meshes
                     .add(Mesh::from(shape::Quad {
-                        size: BORDER_SIZE,
+                        size: BORDER_DIMENSIONS,
                         ..default()
                     }))
                     .into(),
@@ -118,20 +165,18 @@ fn setup(
             .insert(Collider);
     };
 
-    let (max_screen_height, max_screen_width): (i32, i32) = (
-        (SCREEN_HEIGHT / 2f32).floor() as i32,
-        (SCREEN_WIDTH / 2f32).floor() as i32,
-    );
-    let (min_screen_height, min_screen_width): (i32, i32) = (-max_screen_height, -max_screen_width);
-
-    for x in min_screen_width..max_screen_width {
-        border_maker_closure(x as f32, min_screen_height as f32);
-        border_maker_closure(x as f32, max_screen_height as f32);
+    for x in ((MIN_SCREEN_WIDTH.floor() as isize)..(MAX_SCREEN_WIDTH.floor() as isize))
+        .step_by(BORDER_SIZE as usize)
+    {
+        border_maker_closure(x as f32, MIN_SCREEN_HEIGHT);
+        border_maker_closure(x as f32, MAX_SCREEN_HEIGHT);
     }
 
-    for y in min_screen_height..max_screen_height {
-        border_maker_closure(min_screen_width as f32, y as f32);
-        border_maker_closure(max_screen_width as f32, y as f32);
+    for y in ((MIN_SCREEN_HEIGHT.floor() as isize)..(MAX_SCREEN_HEIGHT.floor() as isize))
+        .step_by(BORDER_SIZE as usize)
+    {
+        border_maker_closure(MIN_SCREEN_WIDTH, y as f32);
+        border_maker_closure(MAX_SCREEN_WIDTH, y as f32);
     }
 }
 
@@ -152,29 +197,56 @@ fn move_snake(mut query: Query<(&mut Transform, &Snake)>) {
 
 fn check_collisions(
     snake: Query<&Transform, With<Snake>>,
-    colliders: Query<&Transform, With<Collider>>,
-    mut collision_event_writer: EventWriter<CollisionEvent>
+    colliders: Query<(&Transform, Option<&Bonus>), With<Collider>>,
+    mut collision_event_writer: EventWriter<CollisionEvent>,
 ) {
     let snake_position = snake.single().translation;
-    for collider in colliders.iter() {
+    for (collider, maybe_bonus) in colliders.iter() {
         let collide = collide(
             snake_position,
-            SNAKE_SIZE,
+            SNAKE_DIMENSIONS,
             collider.translation,
-            BORDER_SIZE,
+            BORDER_DIMENSIONS,
         );
         if collide.is_some() {
-            collision_event_writer.send_default();
+            if let Some(bonus) = maybe_bonus {
+                let points = match bonus.extra_bonus {
+                    true => 5,
+                    false => 1,
+                };
+                collision_event_writer.send(CollisionEvent::Bonus(points));
+            } else {
+                collision_event_writer.send_default();
+            }
         }
     }
 }
 
-fn game_over(
+fn collision_handler(
     mut exit: EventWriter<AppExit>,
-    collision_event_reader: EventReader<CollisionEvent>
+    mut collision_event_reader: EventReader<CollisionEvent>,
+    mut bonus: Query<&mut Transform, With<Bonus>>,
+    mut snake: Query<&mut Snake>,
 ) {
     if !collision_event_reader.is_empty() {
-        exit.send(AppExit);
+        for event in collision_event_reader.iter() {
+            match event {
+                CollisionEvent::Bonus(points) => {
+                    let mut bonus_position = bonus.single_mut();
+                    bonus_position.translation = compute_random_translation_inside(
+                        MIN_SCREEN_WIDTH + BORDER_SIZE,
+                        MAX_SCREEN_WIDTH - BORDER_SIZE,
+                        MIN_SCREEN_HEIGHT + BORDER_SIZE,
+                        MAX_SCREEN_HEIGHT - BORDER_SIZE,
+                    );
+                    let mut snake = snake.single_mut();
+                    snake.size += points;
+                }
+                CollisionEvent::Border => {
+                    exit.send(AppExit);
+                }
+            }
+        }
         collision_event_reader.clear();
     }
 }
