568d3207a0a283d2221e0717de1706638530b696
nag763 <loic.labeye@tutanota.de>
2022-10-30 21:33:57 +0100

    Add restart system

--- snake/src/main.rs
+++ snake/src/main.rs
@@ -1,8 +1,7 @@
 use rand::Rng;
 
 use bevy::{
-    app::AppExit, prelude::*, sprite::collide_aabb::collide, sprite::MaterialMesh2dBundle,
-    time::FixedTimestep,
+    prelude::*, sprite::collide_aabb::collide, sprite::MaterialMesh2dBundle, time::FixedTimestep,
 };
 
 /// How many times per seconds the system does an action.
@@ -35,6 +34,15 @@ const BORDER_SIZE: f32 = 15f32;
 const FONT_SIZE: f32 = 10f32;
 /// The score's padding.  
 const FONT_PADDING: Val = Val::Px(1f32);
+
+#[derive(Default, Debug, Eq, PartialEq, Copy, Clone)]
+enum GameState {
+    #[default]
+    Initialized,
+    Running,
+    Paused,
+    Over,
+}
 
 /// The snake direction in a 2D plan
 #[derive(Debug, Eq, PartialEq, Copy, Clone)]
@@ -98,7 +106,7 @@ struct Border;
 #[derive(Debug, Default, Component)]
 struct Collider;
 
-#[derive(Component, Default, Deref, DerefMut)]
+#[derive(Component, Default, Deref, DerefMut, Debug)]
 struct Score(u32);
 
 /// The event following a conflict of position between the snake and a collider.
@@ -138,24 +146,25 @@ fn compute_random_bonus_position() -> Vec3 {
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::BLACK))
-        .insert_resource(Score::default())
+        .init_resource::<Score>()
+        .init_resource::<GameState>()
         .add_plugins(DefaultPlugins)
         .add_event::<CollisionEvent>()
         .add_startup_system(setup)
-        .add_system(window_resize_system)
+        .add_startup_system(window_resize_system)
         .add_system(update_score)
         .add_system_set(
             SystemSet::new()
                 .with_run_criteria(FixedTimestep::step(TIME_STEP))
-                .with_system(collision_handler)
-                .with_system(change_snake_direction)
                 .with_system(check_collisions)
+                .with_system(handle_input)
+                .with_system(move_snake.before(handle_input))
+                .with_system(move_queue.before(move_snake))
                 .with_system(
-                    move_snake
-                        .before(change_snake_direction)
-                        .after(collision_handler),
-                )
-                .with_system(move_queue),
+                    collision_handler
+                        .before(handle_input)
+                        .after(check_collisions),
+                ),
         )
         .run();
 }
@@ -166,7 +175,6 @@ fn setup(
     mut meshes: ResMut<Assets<Mesh>>,
     asset_server: Res<AssetServer>,
 ) {
-    let bonus_initial_position = compute_random_bonus_position();
     // The camera
     commands.spawn_bundle(Camera2dBundle::default());
     commands.spawn_bundle(
@@ -185,35 +193,7 @@ fn setup(
             ..default()
         }),
     );
-    // The snake
-    commands
-        .spawn()
-        .insert(Snake::default())
-        .insert_bundle(MaterialMesh2dBundle {
-            mesh: meshes
-                .add(Mesh::from(shape::Quad {
-                    size: SNAKE_DIMENSIONS,
-                    ..default()
-                }))
-                .into(),
-            transform: Transform::default(),
-            material: materials.add(ColorMaterial::from(Color::WHITE)),
-            ..default()
-        });
-    // The first bonus
-    commands
-        .spawn()
-        .insert(Bonus::default())
-        .insert_bundle(MaterialMesh2dBundle {
-            mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
-            transform: Transform::default()
-                .with_scale(Vec3::splat(BONUS_DIAMETER))
-                .with_translation(bonus_initial_position),
-            material: materials.add(ColorMaterial::from(Color::WHITE)),
-            ..default()
-        })
-        .insert(Collider);
-    // The snake head
+    // The borders
     commands
         .spawn()
         .insert_bundle(MaterialMesh2dBundle {
@@ -233,7 +213,6 @@ fn setup(
             ..default()
         })
         .insert(Collider);
-    // The borders
     commands
         .spawn()
         .insert_bundle(MaterialMesh2dBundle {
@@ -291,6 +270,43 @@ fn setup(
             ..default()
         })
         .insert(Collider);
+    init_game_components(commands, materials, meshes);
+}
+
+fn init_game_components(
+    mut commands: Commands,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+) {
+    let bonus_initial_position = compute_random_bonus_position();
+    // The snake
+    commands
+        .spawn()
+        .insert(Snake::default())
+        .insert_bundle(MaterialMesh2dBundle {
+            mesh: meshes
+                .add(Mesh::from(shape::Quad {
+                    size: SNAKE_DIMENSIONS,
+                    ..default()
+                }))
+                .into(),
+            transform: Transform::default(),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            ..default()
+        });
+    // The first bonus
+    commands
+        .spawn()
+        .insert(Bonus::default())
+        .insert_bundle(MaterialMesh2dBundle {
+            mesh: meshes.add(Mesh::from(shape::Circle::default())).into(),
+            transform: Transform::default()
+                .with_scale(Vec3::splat(BONUS_DIAMETER))
+                .with_translation(bonus_initial_position),
+            material: materials.add(ColorMaterial::from(Color::WHITE)),
+            ..default()
+        })
+        .insert(Collider);
 }
 
 /// Resizes the window at startup.
@@ -302,24 +318,32 @@ fn window_resize_system(mut windows: ResMut<Windows>) {
 /// Updates the displayed score on the screen.                  
 fn update_score(score: Res<Score>, mut query: Query<&mut Text>) {
     let mut text = query.single_mut();
-    text.sections[0].value = score.0.to_string();
+    text.sections[0].value = score.to_string();
 }
 
 /// The movement of snake per TIME_STEP applied to the ball.
-fn move_snake(mut query: Query<(&mut Transform, &mut Snake)>) {
-    let (mut transform, mut snake) = query.single_mut();
-    if let Some(direction) = snake.direction {
-        let translation_diff = direction.into_translation() * SNAKE_SPEED_FACTOR;
-        snake.last_position = transform.translation;
-        transform.translation += translation_diff;
+fn move_snake(mut query: Query<(&mut Transform, &mut Snake)>, game_state: Res<GameState>) {
+    if *game_state == GameState::Running {
+        let (mut transform, mut snake) = query.single_mut();
+        if let Some(direction) = snake.direction {
+            let translation_diff = direction.into_translation() * SNAKE_SPEED_FACTOR;
+            snake.last_position = transform.translation;
+            transform.translation += translation_diff;
+        }
     }
 }
 
-fn move_queue(mut query: Query<&mut Transform, With<Queue>>, snake: Query<&Snake>) {
-    let snake = snake.single();
-    let mut last_position = snake.last_position;
-    for mut transform in query.iter_mut() {
-        std::mem::swap(&mut transform.translation, &mut last_position)
+fn move_queue(
+    mut query: Query<&mut Transform, With<Queue>>,
+    snake: Query<&Snake>,
+    game_state: Res<GameState>,
+) {
+    if *game_state == GameState::Running {
+        let snake = snake.single();
+        let mut last_position = snake.last_position;
+        for mut transform in query.iter_mut() {
+            std::mem::swap(&mut transform.translation, &mut last_position)
+        }
     }
 }
 
@@ -327,25 +351,28 @@ fn check_collisions(
     snake: Query<&Transform, With<Snake>>,
     colliders: Query<(&Transform, Option<&Bonus>), With<Collider>>,
     mut collision_event_writer: EventWriter<CollisionEvent>,
+    game_state: Res<GameState>,
 ) {
-    let snake_position = snake.single().translation;
-    for (collider, maybe_bonus) in colliders.iter() {
-        let collider_dimensions = Vec2::new(collider.scale.x, collider.scale.y);
-        let collide = collide(
-            snake_position,
-            SNAKE_DIMENSIONS,
-            collider.translation,
-            collider_dimensions,
-        );
-        if collide.is_some() {
-            if let Some(bonus) = maybe_bonus {
-                let points = match bonus.extra_bonus {
-                    true => 5,
-                    false => 1,
-                };
-                collision_event_writer.send(CollisionEvent::Bonus(points));
-            } else {
-                collision_event_writer.send_default();
+    if *game_state == GameState::Running {
+        let snake_position = snake.single().translation;
+        for (collider, maybe_bonus) in colliders.iter() {
+            let collider_dimensions = Vec2::new(collider.scale.x, collider.scale.y);
+            let collide = collide(
+                snake_position,
+                SNAKE_DIMENSIONS,
+                collider.translation,
+                collider_dimensions,
+            );
+            if collide.is_some() {
+                if let Some(bonus) = maybe_bonus {
+                    let points = match bonus.extra_bonus {
+                        true => 5,
+                        false => 1,
+                    };
+                    collision_event_writer.send(CollisionEvent::Bonus(points));
+                } else {
+                    collision_event_writer.send_default();
+                }
             }
         }
     }
@@ -355,78 +382,109 @@ fn collision_handler(
     mut commands: Commands,
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
-    mut exit: EventWriter<AppExit>,
     mut collision_event_reader: EventReader<CollisionEvent>,
-    mut bonus: Query<&mut Transform, With<Bonus>>,
+    mut bonus: Query<(&mut Transform, Entity), With<Bonus>>,
     mut score: ResMut<Score>,
+    mut game_state: ResMut<GameState>,
+    snake: Query<Entity, With<Snake>>,
+    queue: Query<Entity, With<Queue>>,
 ) {
-    if !collision_event_reader.is_empty() {
-        for event in collision_event_reader.iter() {
-            match event {
-                CollisionEvent::Bonus(points) => {
-                    // spawn a queue
-                    commands
-                        .spawn()
-                        .insert(Queue)
-                        .insert_bundle(MaterialMesh2dBundle {
-                            mesh: meshes
-                                .add(Mesh::from(shape::Quad {
-                                    size: SNAKE_DIMENSIONS,
-                                    ..default()
-                                }))
-                                .into(),
-                            transform: Transform::default().with_translation(Vec3::new(
-                                MIN_SCREEN_WIDTH,
-                                MIN_SCREEN_HEIGHT,
-                                0f32,
-                            )),
-                            material: materials.add(ColorMaterial::from(Color::GRAY)),
-                            ..default()
-                        })
-                        .insert(Collider);
-                    score.0 += points;
-                    let mut bonus_position = bonus.single_mut();
-                    bonus_position.translation = compute_random_bonus_position();
+    if let Some(event) = collision_event_reader.iter().next() {
+        match event {
+            CollisionEvent::Bonus(points) => {
+                // spawn a queue
+                commands
+                    .spawn()
+                    .insert(Queue)
+                    .insert_bundle(MaterialMesh2dBundle {
+                        mesh: meshes
+                            .add(Mesh::from(shape::Quad {
+                                size: SNAKE_DIMENSIONS,
+                                ..default()
+                            }))
+                            .into(),
+                        transform: Transform::default().with_translation(Vec3::new(
+                            MIN_SCREEN_WIDTH,
+                            MIN_SCREEN_HEIGHT,
+                            0f32,
+                        )),
+                        material: materials.add(ColorMaterial::from(Color::GRAY)),
+                        ..default()
+                    })
+                    .insert(Collider);
+                score.0 += points;
+                let (mut bonus_position, _) = bonus.single_mut();
+                bonus_position.translation = compute_random_bonus_position();
+            }
+            CollisionEvent::Border => {
+                let snake_entity = snake.single();
+                let (_, bonus_entity) = bonus.single();
+                commands.entity(snake_entity).despawn();
+                commands.entity(bonus_entity).despawn();
+                for queue_entity in queue.iter() {
+                    commands.entity(queue_entity).despawn();
                 }
-                CollisionEvent::Border => {
-                    exit.send(AppExit);
-                    println!("Score {}", score.0);
-                    break;
-                }
+                *game_state = GameState::Over;
+                println!("Score {:?}", *score);
             }
         }
-        collision_event_reader.clear();
     }
 }
 
-fn change_snake_direction(keyboard_input: Res<Input<KeyCode>>, mut query: Query<&mut Snake>) {
-    let mut snake = query.single_mut();
+fn handle_input(
+    commands: Commands,
+    materials: ResMut<Assets<ColorMaterial>>,
+    meshes: ResMut<Assets<Mesh>>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut score: ResMut<Score>,
+    mut query: Query<&mut Snake>,
+    mut game_state: ResMut<GameState>,
+) {
+    if keyboard_input.any_just_pressed([KeyCode::Space, KeyCode::P]) {
+        if *game_state == GameState::Paused {
+            *game_state = GameState::Running;
+        } else if *game_state == GameState::Running {
+            *game_state = GameState::Paused;
+        }
+        return;
+    }
+    if *game_state == GameState::Over && keyboard_input.just_pressed(KeyCode::R) {
+        init_game_components(commands, materials, meshes);
+        *game_state = GameState::Initialized;
+        score.0 = 0;
+    }
 
     let mut new_direction: Option<SnakeDirection> = None;
 
-    if keyboard_input.pressed(KeyCode::Right) || keyboard_input.pressed(KeyCode::D) {
+    if keyboard_input.any_pressed([KeyCode::Right, KeyCode::D]) {
         new_direction = Some(SnakeDirection::Right);
     }
-    if keyboard_input.pressed(KeyCode::Left) || keyboard_input.pressed(KeyCode::Q) {
+    if keyboard_input.any_pressed([KeyCode::Left, KeyCode::D]) {
         new_direction = Some(SnakeDirection::Left);
     }
-    if keyboard_input.pressed(KeyCode::Up) || keyboard_input.pressed(KeyCode::Z) {
+    if keyboard_input.any_pressed([KeyCode::Up, KeyCode::Z]) {
         new_direction = Some(SnakeDirection::Up);
     }
-    if keyboard_input.pressed(KeyCode::Down) || keyboard_input.pressed(KeyCode::S) {
+    if keyboard_input.any_pressed([KeyCode::Down, KeyCode::S]) {
         new_direction = Some(SnakeDirection::Down);
     }
+    if *game_state == GameState::Initialized && new_direction.is_some() {
+        *game_state = GameState::Running;
+    }
 
-    // If no current direction => take the value of any new direction
-    if snake.direction.is_none() {
-        snake.direction = new_direction;
-    // If a current direction => ensure there is already a no direction (the
-    // snake can't stop itself)
-    } else if let (Some(current_snake_direction), Some(new_direction_unwrapped)) =
-        (snake.direction, new_direction)
-    {
-        if !current_snake_direction.conflicts_with(new_direction_unwrapped) {
+    if *game_state == GameState::Running {
+        let mut snake = query.single_mut();
+        // If no current direction => take the value of any new direction
+        if snake.direction.is_none() {
             snake.direction = new_direction;
+        // If a current direction => ensure there is already a no direction (the
+        // snake can't stop itself)
+        } else if let (Some(current_snake_direction), Some(new_direction_unwrapped)) =
+            (snake.direction, new_direction)
+        {
+            if !current_snake_direction.conflicts_with(new_direction_unwrapped) {
+                snake.direction = new_direction;
+            }
         }
     }
 }
